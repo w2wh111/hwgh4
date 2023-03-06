8a37677197688f911d2ac526f68b015bba73f13d
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-06 18:49:48 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import Link from 'next/link'
 
 export default function IndexPage() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    process.env.sensitive
   )
 }
